5e05a5ac47c282f96920d1763ceb8a2101367c06
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2013-04-12 16:53:29 -0300

    Created a Test Suite for all Test classes

---
+++ test/br/com/rodrigosasaki/structures/AllTests.java
@@ -0,0 +1,15 @@
+package br.com.rodrigosasaki.structures;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import br.com.rodrigosasaki.structures.bag.BagTest;
+import br.com.rodrigosasaki.structures.iterator.ArrayIteratorTest;
+import br.com.rodrigosasaki.structures.stack.StackTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BagTest.class, ArrayIteratorTest.class, StackTest.class})
+public class AllTests{
+
+}
