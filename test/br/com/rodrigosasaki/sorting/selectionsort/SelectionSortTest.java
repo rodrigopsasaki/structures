b3210108da1a7d25cd2faa3edc172a378d86da88
Rodrigo Perez Sasaki <rodrigopsasaki@gmail.com>
2013-04-13 01:10:56 -0300

    First implementation of Selection Sort

---
+++ test/br/com/rodrigosasaki/sorting/selectionsort/SelectionSortTest.java
@@ -0,0 +1,36 @@
+package br.com.rodrigosasaki.sorting.selectionsort;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * @author Rodrigo Sasaki
+ */
+public class SelectionSortTest{
+
+	/**
+	 * FIXME: Should be implemented with our own utility classes in the future
+	 */
+	@Test
+	public void shouldOrderAShuffledArray(){
+		Integer[] numbers = new Integer[10];
+		
+		numbers[0] = 9;
+		numbers[1] = 8;
+		numbers[2] = 7;
+		numbers[3] = 6;
+		numbers[4] = 5;
+		numbers[5] = 4;
+		numbers[6] = 3;
+		numbers[7] = 2;
+		numbers[8] = 1;
+		numbers[9] = 0;
+		
+		SelectionSort.sort(numbers);
+		
+		for(Integer i = 0 ; i < 10 ; i ++){
+			assertEquals(i, numbers[i]);
+		}
+	}
+}
